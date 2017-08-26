1582dedfdce40fbd9349517ced49e0eeb1bd4845
ioanachi <ioana_chichi@outlook.com>
2017-08-26 22:36:02 +0300

    Added comment in sql config

--- config.php
+++ config.php
@@ -7,3 +7,12 @@ $config = [
      'dbname' => 'hugouiDB',
   ]
 ];
+/*
+CREATE TABLE `users` (
+ `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+ `username` varchar(255) NOT NULL UNIQUE,
+ `email` varchar(255) NOT NULL UNIQUE,
+ `password` varchar(255) NOT NULL,
+ `create_date` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+*/

c7d1049feb090441d3d6259eb36021e6578e71a7
Ciprian Turcu <turcuciprian1@gmail.com>
2017-08-26 09:24:50 +0300

    removed classes, everything is just functions now

--- index.php
+++ index.php
@@ -22,6 +22,6 @@ $container['db'] = function ($c) {
 
 };
 
-require_once('src/rest/rest.php');
+require_once('src/rest/routes.php');
 
 $app->run();
--- src/rest/rest.php
+++
@@ -1,9 +0,0 @@
-<?php
-//include files
-require_once 'db/user.php';
-$dbUser = new dbUser();
-
-require_once 'routes/user.php';
-
-//call the classes
-$rrouteUser = new rrouteUser();

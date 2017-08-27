b8c6dddf21e1c73b499626fc4e0eaaaa184adce0
Ciprian Turcu <turcuciprian1@gmail.com>
2017-08-27 21:56:38 +0300

    updated previous query

--- config.php
+++ config.php
@@ -21,11 +21,11 @@ CREATE TABLE `users` (
 CREATE TABLE `posts` (
  `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
  `post_title` varchar(255) NOT NULL UNIQUE,
- `post_content` text NOT NULL UNIQUE,
+ `post_content` TEXT CHARACTER SET latin1 COLLATE latin1_general_cs,
  `post_status` varchar(50) NOT NULL,
- `post_date` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+ `post_date` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
  `slug` varchar(255) NOT NULL,
  `layout` varchar(255) NOT NULL,
- `url` varchar(255) NOT NULL,
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+ `url` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
 */

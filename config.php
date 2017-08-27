edee9e3db240d86876f0b976e714650704073abe
Ciprian Turcu <turcuciprian1@gmail.com>
2017-08-27 21:50:41 +0300

    Created MySql comand to generate table

--- config.php
+++ config.php
@@ -7,12 +7,25 @@ $config = [
      'dbname' => 'hugouiDB',
   ]
 ];
-/* 
+
+/*
+
 CREATE TABLE `users` (
  `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
  `username` varchar(255) NOT NULL UNIQUE,
  `email` varchar(255) NOT NULL UNIQUE,
  `password` varchar(255) NOT NULL,
  `create_date` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `posts` (
+ `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+ `post_title` varchar(255) NOT NULL UNIQUE,
+ `post_content` text NOT NULL UNIQUE,
+ `post_status` varchar(50) NOT NULL,
+ `post_date` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+ `slug` varchar(255) NOT NULL,
+ `layout` varchar(255) NOT NULL,
+ `url` varchar(255) NOT NULL,
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 */

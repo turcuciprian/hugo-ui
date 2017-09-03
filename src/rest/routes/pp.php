620728401122a617820446f9c2e7ddc7afccc756
ioanachi <ioana_chichi@outlook.com>
2017-09-03 21:52:25 +0300

    Modification

--- src/rest/routes/pp.php
+++ src/rest/routes/pp.php
@@ -1,80 +1,74 @@
 <?php
 
-$app->get('/posts',function($request, $response) {
-   try {
-       $con = $this->db;
-       $sql = "SELECT * FROM pp WHERE type = 'post';";
-       $result = null;
-       $token = $request->getParam('token');
-       $tokenValid = validateToken($token);
+$app->get('/posts', function ($request, $response) {
+    try {
+        $con = $this->db;
+        $sql = "SELECT * FROM pp WHERE type = 'post';";
+        $result = null;
+        $token = $request->getParam('token');
+        $tokenValid = validateToken($token);
 
-       foreach ($con->query($sql) as $row) {
-           $result[] = $row;
-       }
-       if($tokenValid){
+        foreach ($con->query($sql) as $row) {
+            $result[] = $row;
+        }
+        if ($tokenValid) {
+            if ($result) {
+                return $response->withJson(array('status' => 'true', 'result' => $result), 200);
+            } else {
+                return $response->withJson(array('status' => 'Posts Not Found'), 422);
+            }
+        } else {
+            return $response->withJson(array('status' => 'Token invalid'), 422);
+        }
+    } catch (Exception $ex) {
+        return $response->withJson(array('error' => $ex->getMessage()), 422);
+    }
+});//select
+$app->get('/pages', function ($request, $response) {
+    try {
+        $con = $this->db;
+        $sql = "SELECT * FROM pp WHERE type = 'page'";
+        $result = null;
+        $token = $request->getParam('token');
+        $tokenValid = validateToken($token);
+        foreach ($con->query($sql) as $row) {
+            $result[] = $row;
+        }
+        if ($tokenValid) {
+            if ($result) {
+                return $response->withJson(array('status' => 'true', 'result' => $result), 200);
+            } else {
+                return $response->withJson(array('status' => 'Pages Not Found'), 422);
+            }
+        } else {
+            return $response->withJson(array('status' => 'Token invalid'), 422);
+        }
+    } catch (Exception $ex) {
+        return $response->withJson(array('error' => $ex->getMessage()), 422);
+    }
+});// select pages
 
-       if ($result) {
-           return $response->withJson(array('status' => 'true', 'result' => $result), 200);
-       } else {
-           return $response->withJson(array('status' => 'Posts Not Found'), 422);
-       }
-     }else{
-         return $response->withJson(array('status' => 'Token invalid'),422);
-
-       }
-   }catch (Exception $ex) {
-       return $response->withJson(array('error' => $ex->getMessage()), 422);
-   }
-
-});
-$app->get('/pages', function($request, $response) {
-   try {
-       $con = $this->db;
-       $sql = "SELECT * FROM pp WHERE type = 'page'";
-       $result = null;
-       $token = $request->getParam('token');
-       $tokenValid = validateToken($token);
-       foreach ($con->query($sql) as $row) {
-           $result[] = $row;
-       }
-       if($tokenValid){
-
-       if ($result) {
-           return $response->withJson(array('status' => 'true', 'result' => $result), 200);
-       } else {
-           return $response->withJson(array('status' => 'Pages Not Found'), 422);
-       }
-     }else{
-         return $response->withJson(array('status' => 'Token invalid'),422);
-   }
- }catch (Exception $ex) {
-       return $response->withJson(array('error' => $ex->getMessage()), 422);
-   }
-});
-
-$app->post('/pp/{id}', function ($request,$response) {
-   try{
-       $id     = $request->getAttribute('id');
-       $con = $this->db;
-       $sql = "DELETE FROM `pp` WHERE id = :id;";
-       $pre  = $con->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
-       $values = array(
+$app->post('/pp/{id}', function ($request, $response) {
+    try {
+        $id     = $request->getAttribute('id');
+        $con = $this->db;
+        $sql = "DELETE FROM `pp` WHERE id = :id;";
+        $pre  = $con->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+        $values = array(
        ':id' => $id);
-       $result = $pre->execute($values);
-       $token = $request->getParam('token');
-       $tokenValid = validateToken($token);
-       if($tokenValid){
-
-       if($result){
-           return $response->withJson(array('status' => 'Post Deleted'),200);
-       }else{
-           return $response->withJson(array('status' => 'Post Not Found'),422);
-       }
-     }else{
-         return $response->withJson(array('status' => 'Token invalid'),422);
-       }
-   }catch(Exception $ex){
-       return $response->withJson(array('error' => $ex->getMessage()),422);
-   }
-
-});
+        $result = $pre->execute($values);
+        $token = $request->getParam('token');
+        $tokenValid = validateToken($token);
+        if ($tokenValid) {
+            if ($result) {
+                return $response->withJson(array('status' => 'Post Deleted'), 200);
+            } else {
+                return $response->withJson(array('status' => 'Post Not Found'), 422);
+            }
+        } else {
+            return $response->withJson(array('status' => 'Token invalid'), 422);
+        }
+    } catch (Exception $ex) {
+        return $response->withJson(array('error' => $ex->getMessage()), 422);
+    }
+}); //delete posts and pages
